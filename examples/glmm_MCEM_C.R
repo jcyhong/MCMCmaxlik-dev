829b9036808ee4ebff4f05597c2b6f8aff7b2dc4
sastoudt <sstoudt@berkeley.edu>
2021-01-29 09:55:26 -0500

    finish glmm exploration

--- examples/glmm_MCEM_C.R
+++ examples/glmm_MCEM_C.R
@@ -87,6 +87,9 @@ latentNodes <- glmmNew$getNodeNames(latentOnly=TRUE, stochOnly=TRUE)
 
 
 source("~/Desktop/MCMCmaxlik-dev/packages/MCMCmaxlik/R/MCEM_AD_build.R")
+
+#### C ####
+
 C_opts = c(0.001, 0.01, 0.1)
 
 i <- 3
@@ -109,3 +112,92 @@ MCEM_time <- proc.time() - MCEM_time
 #261.607   1.555 265.566 
 # 51.052   0.278  51.060 
 # 11.190   0.259  10.105 
+
+
+### gamma ###
+
+#probability of deciding that the algorithm has converged, that is, that the difference between two Q functions is less than C, when in fact it has not. Default is 0.05.
+
+gamma_opts = c(0.01, 0.05, 0.1) ## don't have to run 2, same as 1 above
+
+i=3
+glmmMCEM <- buildMCEM_AD(
+  model = glmmNew, latentNodes = latentNodes,
+  C = 0.001, gamma = gamma_opts[i]
+)
+
+MCEM_time <- proc.time()
+out <- glmmMCEM$run()
+MCEM_time <- proc.time() - MCEM_time
+
+#beta1      beta2      beta3      beta4       varF       varM 
+# 1.0113609  0.3178907 -1.9495320  0.9938931  1.3942035  1.2328422 
+#1.0137778  0.3236878 -1.9439178  0.9937086  1.3875624  1.2328804 
+#1.0272362  0.3273002 -1.9372581  0.9935502  1.3868738  1.2204953 
+
+
+#user  system elapsed 
+#271.042   2.011 271.950 
+#261.607   1.555 265.566 
+#304.859   1.648 306.624 
+
+### alpha ### 
+
+#probability of a type one error - here, the probability of accepting a parameter estimate that does not increase the likelihood. Default is 0.25.
+
+alpha_opts = c(0.125, 0.25, 0.5) ## don't have to run 2, same as 1 above
+i=3
+glmmMCEM <- buildMCEM_AD(
+  model = glmmNew, latentNodes = latentNodes,
+  C = 0.001, alpha = alpha_opts[i]
+)
+
+MCEM_time <- proc.time()
+out <- glmmMCEM$run()
+MCEM_time <- proc.time() - MCEM_time
+
+#beta1      beta2      beta3      beta4       varF       varM 
+# 1.0292143  0.3299211 -1.9577776  1.0117851  1.4267171  1.2929639 
+#1.0137778  0.3236878 -1.9439178  0.9937086  1.3875624  1.2328804 
+# stopped early because taking forever
+#1.0292143  0.3299211 -1.9577776  1.0117851  1.4267171  1.2929639 
+#Iteration Number: 24468.
+#Convergence Criterion: 0.03552577.
+
+
+#user  system elapsed 
+#199.084   2.434 202.232 
+#261.607   1.555 265.566 
+#stopped at 15156.606    61.307 15183.862 
+
+
+
+### beta ###
+
+#probability of a type two error - here, the probability of rejecting a parameter estimate that does increase the likelihood. Default is 0.25.
+
+beta_opts = c(0.125, 0.25, 0.5) ## don't have to run 2, same as 1 above
+
+i=3
+glmmMCEM <- buildMCEM_AD(
+  model = glmmNew, latentNodes = latentNodes,
+  C = 0.001, beta = beta_opts[i]
+)
+
+MCEM_time <- proc.time()
+out <- glmmMCEM$run()
+MCEM_time <- proc.time() - MCEM_time
+
+
+#beta1      beta2      beta3      beta4       varF       varM 
+#1.0223402  0.3212341 -1.9422735  0.9949244  1.3868114  1.2435892 
+#1.0137778  0.3236878 -1.9439178  0.9937086  1.3875624  1.2328804 
+#1.0206710  0.3246179 -1.9368275  0.9931220  1.3769811  1.2217263 
+
+
+#user  system elapsed 
+#322.663   2.054 330.900 
+#261.607   1.555 265.566 
+#142.684   0.769 143.416 
+
+
